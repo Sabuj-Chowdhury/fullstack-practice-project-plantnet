5551ff9b7b0c2d8cf9ba0bd87c53fd00eca61b57
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-01-11 01:18:53 +0530

    Data fetched for indivual product and show in UI

--- client/src/components/Home/Plants.jsx
+++ client/src/components/Home/Plants.jsx
@@ -12,6 +12,7 @@ const Plants = () => {
       const { data } = await axios.get(
         `${import.meta.env.VITE_API_URL}/plants`
       );
+
       return data;
     },
   });
@@ -21,11 +22,15 @@ const Plants = () => {
   // console.log(plants);
   return (
     <Container>
-      <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {plants.map((plant) => (
-          <Card key={plant._id} plant={plant} />
-        ))}
-      </div>
+      {plants && plants.length > 0 ? (
+        <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {plants.map((plant) => (
+            <Card key={plant._id} plant={plant} />
+          ))}
+        </div>
+      ) : (
+        <p>No Data available</p>
+      )}
     </Container>
   );
 };
